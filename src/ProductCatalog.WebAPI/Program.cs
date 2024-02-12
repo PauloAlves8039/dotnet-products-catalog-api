665414889665f794f96434affb757b4964134d5f
PauloAlves8039 <pj38alves@gmail.com>
2024-02-12 15:28:36 -0300

    Defininco configurações para o banco de dados

--- src/ProductCatalog.WebAPI/Program.cs
+++ src/ProductCatalog.WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using ProductCatalog.Infra.IoC.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 
