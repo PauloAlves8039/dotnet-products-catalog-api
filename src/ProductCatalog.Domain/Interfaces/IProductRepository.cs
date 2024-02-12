871484a8d404d7233f3fb7ba838aef5f64d64230
PauloAlves8039 <pj38alves@gmail.com>
2024-02-12 14:35:07 -0300

    Criando elementos no modelo de domínio

---
+++ src/ProductCatalog.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetById(int? id);
+
+        Task<Product> Create(Product product);
+
+        Task<Product> Update(Product product);
+
+        Task<Product> Remove(Product product);
+    }
+}
